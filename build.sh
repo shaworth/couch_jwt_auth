53598aded3de51ce5c0684b7a20bacd42f0ab5a3
Shannon Haworth <shannon.haworth@gmail.com>
2016-07-30 02:58:56 -0400

    Update build.sh
    switched order of cp command to prevent the priv cp from causing the script to exit non-zero.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rebar get-deps co
 # fix some incompatibility with couchdb plugins and rebar deps
-for file in deps/*/ebin/*; do cp "$file" "ebin/$(basename $file)";done
 for file in deps/*/priv/*.so; do cp "$file" "priv/$(basename $file)";done
+for file in deps/*/ebin/*; do cp "$file" "ebin/$(basename $file)";done
